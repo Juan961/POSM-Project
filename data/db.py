565263a4d165807d952b1667859b3cca408161fe
Juan Jose <juanjo5792@gmail.com>
2023-05-27 14:33:09 -0500

    Format code

--- data/db.py
+++ data/db.py
@@ -1,12 +1,16 @@
 import sqlite3
 
+
 conn = sqlite3.connect("./data/db.db")
+
 
 def get_conn():
     return conn
 
+
 def get_cursor():
     return conn.cursor()
+
 
 def commit_db():
     return conn.commit()
