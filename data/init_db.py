bc586e641a4dbbb7adfce04ea40536fdaaef64ff
Juan Jose <juanjo5792@gmail.com>
2023-05-27 14:27:04 -0500

    Update init_db

--- data/init_db.py
+++ data/init_db.py
@@ -1,21 +1,29 @@
 from db import get_conn
 
+
 def init_database(conn):
-    with open('./data/script.sql', 'r') as f:
-        script = f.read()
+    try:
+        with open('./data/script.sql', 'r') as f:
+            script = f.read()
 
-    # Split the script into individual statements
-    statements = script.split(';')
+        # Split the script into individual statements
+        statements = script.split(';')
 
-    # Remove any empty statements
-    statements = [stmt.strip() for stmt in statements if stmt.strip()]
+        # Remove any empty statements
+        statements = [stmt.strip() for stmt in statements if stmt.strip()]
 
-    # Execute each statement
-    for stmt in statements:
-        conn.execute(stmt)
+        # Execute each statement
+        for stmt in statements:
+            conn.execute(stmt)
 
-    conn.commit()
-    conn.close()
+        conn.commit()
+        conn.close()
+    
+        print("Executed correctly")
+
+    except:
+        print("Executed failed")
+
 
 if __name__ == "__main__":
     warning = input("By executing this script you are going to delete all the data in the DB. Are you sure to continue? y/N: ")
